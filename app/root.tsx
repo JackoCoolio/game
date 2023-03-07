3ed90f8b770be7380091df8d8876bda8c6a6ad01
Jackson Wambolt <jtwambolt@gmail.com>
2023-03-07 03:28:05 -0600

    Add global.css

--- app/root.tsx
+++ app/root.tsx
@@ -8,11 +8,17 @@ import {
   ScrollRestoration,
 } from "@remix-run/react"
 
+import styles from "./styles/global.css"
+
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Game",
   viewport: "width=device-width,initial-scale=1",
 })
+
+export const links = () => {
+    return [{ rel: "stylesheet", href: styles }]
+}
 
 export default function App() {
   return (
