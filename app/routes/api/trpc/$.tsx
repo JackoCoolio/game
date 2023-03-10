f3227c061a957b010cd58a832eb5b6ced2973628
Jackson Wambolt <jtwambolt@gmail.com>
2023-03-09 22:25:08 -0600

    Fix TRPC error

--- app/routes/api/trpc/$.tsx
+++ app/routes/api/trpc/$.tsx
@@ -1,13 +1,19 @@
-import { requestHandler } from "~/lib/trpc/router"
+import { DataFunctionArgs } from "@remix-run/node"
 
-/**
- * Remix makes a distinction between GET and non-GET requests, so we have two
- * functions here. `loader` and `action` are used by Remix; We don't import
- * them anywhere.
- */
+/*
+Remix makes a distinction between GET and non-GET requests, so we have two
+functions here. `loader` and `action` are used by Remix; We don't import
+them anywhere.
+*/
 
 // handle GET requests
-export const loader = requestHandler
+export const loader = async (args: DataFunctionArgs) => {
+  const { requestHandler } = await import("~/lib/trpc/router")
+  return requestHandler(args)
+}
 
 // handle non-GET requests
-export const action = requestHandler
+export const action = async (args: DataFunctionArgs) => {
+  const { requestHandler } = await import("~/lib/trpc/router")
+  return requestHandler(args)
+}
